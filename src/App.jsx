4ccd463a7979c02051e8668ae01255d1fbdf85c9
Himanshu kaushik <rtxhoney@duck.com>
2023-10-07 00:09:15 +0530

    created folder and api calling functions and added multi components
    now designing part left designs are ready just need to implement into the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect} from 'react'
+
+import { fetchDataFromApi } from './utils/api.js';
+
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+const dispatch = useDispatch();
+const { url }= useSelector((state) => state.home);
+  console.log(url);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(()=>{
+    apiTesting();
+  }, []);
+
+
+  const apiTesting = () =>{
+    fetchDataFromApi('/movie/popular').then((res)=>{
+      console.log(res);
+      dispatch(getApiConfiguration(res))
+    }) 
+  }
+  return (
+    <div className="App">App
+    {url?.total_pages}
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.scss";
+
+const footer = () => {
+  return (
+    <div>footer</div>
+  )
+}
+
+export default footer
