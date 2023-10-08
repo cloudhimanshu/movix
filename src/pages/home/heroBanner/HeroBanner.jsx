d21468383b1bc2f86880417a93bb2fe36159f6d5
Himanshu kaushik <rtxhoney@duck.com>
2023-10-08 19:46:14 +0530

    new updates

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -18,7 +18,7 @@ const HeroBanner = () => {
     useEffect(() => {
         const bg =
             url.backdrop +
-            data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+            data?.results?.[Math.floor(Math.random() * 10)]?.backdrop_path;
         setBackground(bg);
     }, [data]);
 
@@ -36,13 +36,12 @@ const HeroBanner = () => {
                 </div>
             )}
 
-            <div className="opacity-layer"></div>
+            <div className="opacity-layer"></div> 
             <ContentWrapper>
                 <div className="heroBannerContent">
-                    <span className="title">Welcome.</span>
+                    <span className="title">RTX MOVIES</span>
                     <span className="subTitle">
                         Millions of movies, TV shows and people to discover.
-                        Explore now.
                     </span>
                     <div className="searchInput">
                         <input
